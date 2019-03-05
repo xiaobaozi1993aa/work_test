9fcb43edf6fe3cece38d521bcd70bd37262c83fb
fangzangen <g318xiaobao@163.com>
2019-03-05 17:13:38 +0800

    logging调用

---
+++ config_kshb.py
@@ -0,0 +1,46 @@
+'''
+@1.logging类封装；
+@2.port,sql,linux,redis方法封装；
+'''
+# coding:utf-8
+import logging
+import time
+
+class TestLog(object):
+
+    def __init__(self, logger=None):
+
+        # 创建一个logger
+        self.logger = logging.getLogger(logger)
+        self.logger.setLevel(logging.DEBUG)
+        # 创建一个handler，用于写入日志文件
+        self.log_time = time.strftime("%Y_%m_%d_")
+        self.log_path = "D:\\快收\\ks_log\\"
+        self.log_name = self.log_path + self.log_time + 'test.log'
+        fh = logging.FileHandler(self.log_name, 'w', encoding='utf-8') # a是追加模式
+        fh.setLevel(logging.INFO)
+
+        # 再创建一个handler，用于输出到控制台
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.ERROR)
+
+        # 定义handler的输出格式
+        formatter = logging.Formatter(
+            '[%(asctime)s] %(filename)s->%(funcName)s line:%(lineno)d [%(levelname)s]%(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+
+        # 给logger添加handler
+        self.logger.addHandler(fh)
+        self.logger.addHandler(ch)
+
+        #  添加下面一句，在记录日志之后移除句柄
+        # self.logger.removeHandler(ch)
+        # self.logger.removeHandler(fh)
+        # 关闭打开的文件
+        fh.close()
+        ch.close()
+
+    def getlog(self):
+        return self.logger
+
